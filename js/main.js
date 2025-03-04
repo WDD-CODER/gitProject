fd2e8f7d28ed459b94f57ddc772f8169e4b3e7aa
Your Name <dan.weibren@gmail.com>
2025-03-04 20:48:42 +0200

    Add ball grow function

--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
 'use strict';
+// 10.Change onBallClick() to increment the ball’s width & height by 50px and
 
-function onBallClick(){
-    console.log('hi there!');
+// function onBallClick(){
+//     const elBall = document.querySelector('.ball')
+//     var el = elBall.style.height
+//     console.log(el)
+//     elBall.style.width += '50px'
+//     console.log('hi there!');
+// }
+
+function onBallClick() {
+    const elBall = document.querySelector('.ball');
+    if (!elBall) return
+    var currHeight = parseInt(getComputedStyle(elBall).height)
+    var currWidth = parseInt(getComputedStyle(elBall).width)
+
+
+    elBall.style.height = (currHeight + 50) + "px";
+    var width = elBall.style.width = (currWidth + 50) + "px";
+    elBall.innerText = width
+    
+    console.log("🚀 Updated height:", elBall.style.height);
+    console.log("🚀 Updated width:", elBall.style.width);
 }
